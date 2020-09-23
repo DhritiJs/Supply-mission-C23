15a0b0e1dd6bb37509899892869e729418d2248b
DhritiJs <70759231+DhritiJs@users.noreply.github.com>
2020-09-23 13:07:58 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class box{
+constructor(x,y,width,height){
+    var options={
+        isStatic:true
+    }
+
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.height=height;
+    this.width=width;
+  
+}
+display(){
+    var pos=this.body.position
+    rectMode(CENTER);
+    fill ("red");
+    rect(pos.x,pos.y,this.width,this.height);
+ }
+}
